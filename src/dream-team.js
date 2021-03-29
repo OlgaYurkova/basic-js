b42a8f178004440134d7a14d89b227d46c93724e
OlgaYurkova <olya_yurkovaa@mail.ru>
2021-03-29 21:44:53 +0300

    feat: add carbon-dating, count-cats, dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if (!Array.isArray(members)) {
+		return false;
+	}
+
+	let nameDreamTeam = "";
+	let letter;
+
+	for (let i = 0; i < members.length; i++) {
+		if (typeof members[i] === 'string') {
+			letter = members[i].trim().slice(0, 1);
+			nameDreamTeam = nameDreamTeam.concat(letter);
+		}
+	}
+
+	return nameDreamTeam.toUpperCase().split('').sort().join('');
 };
